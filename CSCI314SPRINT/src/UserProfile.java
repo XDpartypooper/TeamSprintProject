047eb31fce7c3a40a0e68ab257a1f5de1fe5995e
XDpartypooper <115786457+XDpartypooper@users.noreply.github.com>
2022-10-15 21:01:01 +0800

    Add files via upload
    netbeans project file

---
+++ CSCI314SPRINT/src/UserProfile.java
@@ -0,0 +1,15 @@
+/*
+ This is class for profiles
+ e.g sys admin, author 
+ */
+
+/**
+  @author XDpartypooper
+ */
+public class UserProfile {
+    
+    //SystemAdmin
+    //ConferenceChair
+    //Reviewer
+    //Author
+}

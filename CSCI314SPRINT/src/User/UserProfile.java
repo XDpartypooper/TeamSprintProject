95039d7e24d775cfdc33b188950beeacbf465b97
XDpartypooper <115786457+XDpartypooper@users.noreply.github.com>
2022-10-18 23:38:15 +0800

    net beans code
    18/10/22 -Roy

---
+++ CSCI314SPRINT/src/User/UserProfile.java
@@ -0,0 +1,26 @@
+package User;
+
+/*
+The data for the current user 
+superclass for ConChair,reviwer,systemadmin,author
+ */
+
+/**
+  @author XDpartypooper
+ */
+public class UserProfile {
+    String UserName;
+    String Password;
+    String Email;
+    String ProfileType;
+    //1=Author , 2=ConChair, 3=Reviwer, 4=SystemAdmin
+    
+    public UserProfile()
+    {
+        
+    }
+    //SystemAdmin
+    //ConChair
+    //Reviewer
+    //Author
+}

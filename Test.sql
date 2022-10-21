b08181305894c7563a3276fa45069c7e74bfcba8
XDpartypooper <115786457+XDpartypooper@users.noreply.github.com>
2022-10-21 15:59:25 +0800

    Update Test.sql

--- Test.sql
+++ Test.sql
@@ -1,22 +1,55 @@
-DROP TABLE if exists Users;
+create database sprint;-- to create the db
 
+DROP TABLE if exists Users;
 -- CREATE TABLE users
 CREATE TABLE Users  (
 	UserName				VARCHAR(20)		NOT NULL,
 	Password				VARCHAR(20)		NOT NULL,
 	Email					VARCHAR(50)		NOT NULL,
-	ProfileType 				VARCHAR(15)		NOT NULL,
+	ProfileType 				VARCHAR(30)		NOT NULL,
 	ID   		 			VARCHAR(4)		NOT NULL,
 
 CONSTRAINT users_PKEY PRIMARY KEY (UserName, ID)
 );
-
 -- insert dummy data for the 4 profiles
-INSERT INTO Users VALUES ('Roy','Password1','Bruh1@uow.com','4','001');
-INSERT INTO Users VALUES ('Author','Password2','Bruh2@uow.com','1','002');
-INSERT INTO Users VALUES ('ConChair','Password3','Bruh3@uow.com','2','003');
-INSERT INTO Users VALUES ('Reviewer','Password4','Bruh4@uow.com','3','004');
+INSERT INTO Users VALUES ('Roy','Password1','Bruh1@uow.com','SystemAdmin','001');
+INSERT INTO Users VALUES ('Author','Password2','Bruh2@uow.com','Author','002');
+INSERT INTO Users VALUES ('ConChair','Password3','Bruh3@uow.com','Conference Chair','003');
+INSERT INTO Users VALUES ('Reviewer','Password4','Bruh4@uow.com','Reviwer','004');
+
+DROP TABLE if exists usertype;
+
+-- CREATE TABLE usertype
+CREATE TABLE usertype  (
+	ProfileType 				VARCHAR(30)		NOT NULL,
+CONSTRAINT users_PKEY PRIMARY KEY (ProfileType)
+);
+-- insert dummy data for the 4 profiles
+INSERT INTO usertype VALUES ('SystemAdmin');
+INSERT INTO usertype VALUES ('Author');
+INSERT INTO usertype VALUES ('Conference Chair');
+INSERT INTO usertype VALUES ('Reviwer');
 
 select * from users; -- view table users
+select * from usertype; 
 
-create database sprint;-- to create the db
+
+
+
+
+
+--below is testing data for papers
+CREATE TABLE Papers  (
+	PaperName				VARCHAR(50)		NOT NULL,
+	AuthorID   		 		VARCHAR(4)		NOT NULL, 
+	allocatedID				VARCHAR(4),
+	SubmittedDate			VARCHAR(20)		NOT NULL,
+	
+	--Email					VARCHAR(50)		NOT NULL,
+	--ProfileType 			VARCHAR(15)		NOT NULL,
+	
+CONSTRAINT Papers_PKEY PRIMARY KEY (UserName, ID)
+FOREIGN KEY (PersonID) REFERENCES Persons(PersonID)
+
+CONSTRAINT Papers_FKEY FOREIGN KEY (AuthorID) REFERENCES users(ID)
+);

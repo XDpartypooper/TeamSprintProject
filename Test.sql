c6456d67a545a1e321bef596efd398909ae5ef10
XDpartypooper <115786457+XDpartypooper@users.noreply.github.com>
2022-11-09 20:18:13 +0800

    Add files via upload

--- Test.sql
+++ Test.sql
@@ -11,28 +11,13 @@ CREATE TABLE Users  (
 
 CONSTRAINT users_PKEY PRIMARY KEY (UserName, ID)
 );
--- insert dummy data for the 4 profiles
-INSERT INTO Users VALUES ('Roy','Password1','Bruh1@uow.com','System Admin','001');
-INSERT INTO Users VALUES ('Author','Password2','Bruh2@uow.com','Author','002');
-INSERT INTO Users VALUES ('ConChair','Password3','Bruh3@uow.com','Conference Chair','003');
-INSERT INTO Users VALUES ('Reviewer','Password4','Bruh4@uow.com','Reviewer','004');
-INSERT INTO Users VALUES ('Author2','Password2','Bruh5@uow.com','Author','005');
-INSERT INTO Users VALUES ('Reviewer2','Password4','Bruh4@uow.com','Reviewer','006');
-
 DROP TABLE if exists usertype;
-
 -- CREATE TABLE usertype
 CREATE TABLE usertype  (
 	ProfileType 				VARCHAR(30)		NOT NULL,
 	ProfileType_ID   		 	VARCHAR(4)		NOT NULL,
 CONSTRAINT users_PKEY PRIMARY KEY (ProfileType,ProfileType_ID)
 );
--- insert dummy data for the 4 profiles
-INSERT INTO usertype VALUES ('System Admin','1');
-INSERT INTO usertype VALUES ('Author','2');
-INSERT INTO usertype VALUES ('Conference Chair','3');
-INSERT INTO usertype VALUES ('Reviwer','4');
-
 -- Papers 
 -- test for papers with file
 DROP TABLE if exists Papers;
@@ -53,13 +38,6 @@ CREATE TABLE Reviewer  (
 	WorkLoad				    VARCHAR(4),
 CONSTRAINT Reviewer_PKEY PRIMARY KEY (ReviewerID)
 );
-INSERT INTO Reviewer VALUES ('004', null);
--- dummy value for author 
-INSERT INTO papers VALUES ('Lusty Argonian Maid Folio','002',null,'1', null,null,'2011-11-11');
-INSERT INTO papers VALUES ('RoRos bizare adventure','005',null,'2', null,null,'1998-09-09');
-INSERT INTO papers VALUES ('Idoit guides to being a Conchair','002',null,'3', null,null,'2022-01-01');
-INSERT INTO papers VALUES ('101 reasons why CSCI251 is horrible','005',null,'4', null,null,'2022-07-27');
--- dummy papers has no txt tag to it
 -- bids 
 DROP TABLE if exists Bids;
 CREATE TABLE Bids  (
@@ -76,6 +54,31 @@ CREATE TABLE Reviews  (
 	Rating						int				, -- null means pending or deleted currently
 CONSTRAINT Reviews_PKEY PRIMARY KEY (PaperID)
 );
+
+-- dummy data
+
+-- insert dummy data for the 4 profiles
+INSERT INTO usertype VALUES ('System Admin','1');
+INSERT INTO usertype VALUES ('Author','2');
+INSERT INTO usertype VALUES ('Conference Chair','3');
+INSERT INTO usertype VALUES ('Reviwer','4');
+
+-- insert dummy data for the 4 profiles
+INSERT INTO Users VALUES ('Roy','Password1','Bruh1@uow.com','System Admin','001');
+INSERT INTO Users VALUES ('Author','Password2','Bruh2@uow.com','Author','002');
+INSERT INTO Users VALUES ('ConChair','Password3','Bruh3@uow.com','Conference Chair','003');
+INSERT INTO Users VALUES ('Reviewer','Password4','Bruh4@uow.com','Reviewer','004');
+INSERT INTO Users VALUES ('Author2','Password2','Bruh5@uow.com','Author','005');
+INSERT INTO Users VALUES ('Reviewer2','Password4','Bruh4@uow.com','Reviewer','006');
+
+-- dummy data for reviewer work load 
+INSERT INTO Reviewer VALUES ('004', null);
+-- dummy value for author 
+INSERT INTO papers VALUES ('Lusty Argonian Maid Folio','002',null,'1', null,null,'2011-11-11');
+INSERT INTO papers VALUES ('RoRos bizare adventure','005',null,'2', null,null,'1998-09-09');
+INSERT INTO papers VALUES ('Idoit guides to being a Conchair','002',null,'3', null,null,'2022-01-01');
+INSERT INTO papers VALUES ('101 reasons why CSCI251 is horrible','005',null,'4', null,null,'2022-07-27');
+-- dummy papers has no txt tag to it
 -- to here
 
 
@@ -94,13 +97,5 @@ select * from Bids;
 -- testing stuff below
 
 
-
-
--- comment
-CREATE TABLE comments  (
-	PaperID     		 		VARCHAR(4)		NOT NULL, 
-
-CONSTRAINT Comment_PKEY PRIMARY KEY (PaperID)
-);
 
 
